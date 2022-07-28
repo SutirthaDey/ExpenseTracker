6d1aa2f30cc6cd30149e90865a44c0e4daffa810
SutirthaDey <deysutirtha31@gmail.com>
2022-07-28 14:04:29 +0530

    added sign.up html

---
+++ index.js
@@ -0,0 +1,10 @@
+const signUpForm = document.querySelector('.signup-form');
+    
+signUpForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    // console.log(e.target.name.value);
+    // console.log(e.target.email.value);
+    // console.log(e.target.phone.value);
+    // console.log(e.target.password.value);
+})
